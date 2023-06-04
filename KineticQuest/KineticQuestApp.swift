4eaddd22e10e7327857fbbd97adb6cbeeb91b8c1
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-06-04 22:13:20 +1000

    Initial Commit

---
+++ KineticQuest/KineticQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KineticQuestApp.swift
+//  KineticQuest
+//
+//  Created by Wasif Kamruddin on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct KineticQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
