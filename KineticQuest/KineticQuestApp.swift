9d20b2a4d573653ec71b303b2cda5c22a46f311e
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-06-04 23:15:02 +1000

    changed to make it successfully build plus added annimation and background music.

--- KineticQuest/KineticQuestApp.swift
+++ KineticQuest/KineticQuestApp.swift
@@ -6,12 +6,27 @@
 //
 // KineticQuestApp.swift
 import SwiftUI
+import AVFoundation
 
 @main
 struct KineticQuestApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+
+    var audioPlayer: AVAudioPlayer?
+
+    init() {
+        let sound = Bundle.main.path(forResource: "backgroundMusic", ofType: "mp3")
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+            audioPlayer?.numberOfLoops = -1
+            audioPlayer?.play()
+        } catch {
+            print("Error: audio file not found")
         }
     }
 }
